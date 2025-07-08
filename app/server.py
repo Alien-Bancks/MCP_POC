6c20fd02e0e363607a6dc2886fd8d0f682092eb8
leidery <alinebancks2@gmail.com>
2025-07-08 14:32:12 -0300

    envio

--- app/server.py
+++ app/server.py
@@ -1,6 +1,5 @@
 from mcp.server.fastmcp import FastMCP
 
-
 mcp = FastMCP("mcp")
 
 @mcp.tool()
@@ -8,10 +7,16 @@ def somar(a: int, b: int) -> int:
     """Soma dois números"""
     return a + b
 
+@mcp.tool()
+def subtrair(a: int, b: int) -> int:
+    """Subtrai dois números"""
+    return a - b    
+
+
 @mcp.resource("frase://batata-doce")
 def receita() -> str:
     """Retorna uma frase sobre batata doce"""
     return "Para fazer batata doce: asse no forno com azeite por 40 minutos."
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio",port="3001")
+    mcp.run(transport="stdio", port=3001)
--- client/client.py
+++ client/client.py
@@ -1,2 +1,12 @@
-import requests
+import requests 
+from mcp.client import MCPClient
+
+
+def main():
+
+    client = MCPClient("http://localhost:3001")
+    resposta = client.call("somar", a=1, b=2)
+    print(f"Resposta da soma: {resposta}")
+
+
 
